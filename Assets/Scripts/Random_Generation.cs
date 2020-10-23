c62828bde7a959d058cd1c72c6a2e64783195c5b
SergiuGlingeanu <s.glingeanu@gmail.com>
2020-10-23 03:02:57 +0100

    Some what random generation

---
+++ Assets/Scripts/Random_Generation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Random_Generation : MonoBehaviour
+{
+    public int roomsToGenerate;
+
+    private int _roomsSpawned;
+
+    public float chanceToSpawnRooms;
+
+    public GameObject room, corridor;
+
+    void Start()
+    {
+        Instantiate(room, transform.position, Quaternion.identity);
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (Random.Range(0, 100) < chanceToSpawnRooms && _roomsSpawned < roomsToGenerate)
+            {
+                GenerateCorridor(i * 90, transform.position);
+                _roomsSpawned++;
+            } 
+        }
+
+        GenerateCorridor(90, transform.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void GenerateCorridor(float _rotation, Vector3 _location)
+    {
+        GameObject cor = Instantiate(corridor, _location, Quaternion.Euler(0, 0, _rotation));
+
+        Transform[] _tran = cor.GetComponentsInChildren<Transform>();
+
+        GenerateRoom(_tran[1].position);
+    }
+
+    private void GenerateRoom(Vector3 _location)
+    {
+        GameObject big = Instantiate(room, _location, new Quaternion());
+        if (_roomsSpawned < roomsToGenerate)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (Random.Range(0, 100) < chanceToSpawnRooms && _roomsSpawned < roomsToGenerate)
+                {
+                    GenerateCorridor(i * 90, big.GetComponent<Transform>().position);
+                    _roomsSpawned++;
+                }
+            }
+        }
+    }
+}
