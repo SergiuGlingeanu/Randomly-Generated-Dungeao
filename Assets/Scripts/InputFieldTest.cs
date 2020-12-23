22cfff44ce4db3791719c195f33c6970adae1119
SergiuGlingeanu <s.glingeanu@gmail.com>
2020-12-23 15:08:58 +0000

    Added UI

---
+++ Assets/Scripts/InputFieldTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InputFieldTest : MonoBehaviour
+{
+    public Basic_Script basicScript;
+    public Random_Generation_V2 generationScript;
+    public GameObject room;
+
+    public static InputFieldTest Singleton = null;
+
+    public int yes = 0;
+
+    private void Awake()
+    {
+        if (Singleton == null) Singleton = this;
+        else Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        room = GameObject.Find("Room V2.0");
+        generationScript = room.GetComponent<Random_Generation_V2>();
+
+        generationScript.roomsGenerate = yes;
+    }
+
+    public void Yes(int number)
+    {
+        Debug.Log("called");
+        yes = number;
+        if (yes != 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scripts/SetRoomNumber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRoomNumber : MonoBehaviour
+{
+    private GameObject gameContr;
+
+    public GameObject room;
+    public Random_Generation_V2 randomGen;
+
+    public void Start()
+    {
+        gameContr = GameObject.Find("Game Controller");
+    }
+
+    public void SetRoomNum(string num)
+    {
+        int rum = int.Parse(num);
+        if (rum != 0)
+        {
+            gameContr.GetComponent<InputFieldTest>().Yes(rum);
+            Debug.Log("maybe");
+        }
+    }
+}
